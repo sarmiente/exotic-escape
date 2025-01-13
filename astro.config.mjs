3cb8ec9eb29f4d30310e6c2c409f81d53dc304c3
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-01-12 22:41:50 -0500

    committsss

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: "http://localhost:4321/",
   image: {
     remotePatterns: [{ protocol: "https" }],
