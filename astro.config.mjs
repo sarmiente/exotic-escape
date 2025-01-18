844dc394e526d606e12c4af57e70319beb1de9b2
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-01-18 01:07:57 -0500

    commitse

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: "https://wesell.com.co",
   image: {
     remotePatterns: [{ protocol: "https" }],
