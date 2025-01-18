c57a6bf0cc0f1e46e61fedda3dcef7ba264a16e4
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-01-17 19:51:33 -0500

    commit a lot again mostly finish

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -7,17 +7,13 @@ export default  {
       es: {
         "nosotros": "nosotros",
         "blog" : "blog",
-        "quienes-somos": "quienes-somos",
-        "nuestro-equipo": "nuestro-equipo",
         "contacto": "contacto",
         "politica-de-privacidad": "politica-de-privacidad",
-        "terminos-y-condiciones" : "terminos-y-condiciones",        
+        "terminos-y-condiciones":"terminos-y-condiciones",        
       },
       en: {
         "nosotros": "about",
         "blog" : "blog",
-        "quienes-somos": "who-we-are",
-        "nuestro-equipo": "meet-our-team",
         "contacto": "contact",
         "politica-de-privacidad": "privacy-policy",
         "terminos-y-condiciones" : "terms-and-conditions",
