575fa0f4b1e12b806765651368e36777d673e3cc
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-01-24 10:06:05 -0500

    Web3Form implementation for the forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,10 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
-import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
   site: "https://wesell.com.co",
   image: {
     remotePatterns: [{ protocol: "https" }],
